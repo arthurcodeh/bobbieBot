99809ae4a50f397eb5f26a46920fd552e2ed2aed
arthurcodeh <129370647+arthurcodeh@users.noreply.github.com>
2026-02-08 17:02:54 +0100

    [Feat:arduino] Add RobotMain module with initialization and main loop structure

---
+++ src/uno/core/RobotMain.h
@@ -0,0 +1,9 @@
+#ifndef BOBBIEBOT_ROBOTMAIN_H
+#define BOBBIEBOT_ROBOTMAIN_H
+
+namespace RobotMain {
+    class RobotMain {
+    };
+} // RobotMain
+
+#endif //BOBBIEBOT_ROBOTMAIN_H
--- src/uno/main.cpp
+++ src/uno/main.cpp
@@ -1,7 +1,14 @@
+// File: main.cpp
+// Role : Point d'entrée principal du programme, gère l'initialisation et la boucle
+// Description : Ce fichier contient le point d'entrée principal du programme, qui gère l'initialisation du robot et la boucle principale pour le contrôle du robot.
+// Il inclut les fichiers de configuration et de communication nécessaires pour le fonctionnement du robot.
+
 #include <Arduino.h>
 
 void setup() {
+
 }
 
 void loop() {
+
 }
