8e65c39f838d1a0df4f71a884aa290bc9badeab7
arthurcodeh <129370647+arthurcodeh@users.noreply.github.com>
2026-02-08 17:03:02 +0100

    [Feat:arduino] Add SerialProtocol module for Arduino-ESP32 communication

---
+++ src/uno/communication/SerialProtocol.cpp
@@ -0,0 +1,11 @@
+// File : SerialProtocol.cpp
+// Role: Communication entre l'arduino et l'esp 32
+
+// Description : SerialProtocol sert à gérer la communication entre l'arduino et l'esp32,
+// qui transmet les données à l'arduino pour le contrôle du robot
+
+
+#include "SerialProtocol.h"
+
+namespace SerialProtocol {
+} // SerialProtocol
---
+++ src/uno/communication/SerialProtocol.h
@@ -0,0 +1,9 @@
+#ifndef BOBBIEBOT_SERIALPROTOCOL_H
+#define BOBBIEBOT_SERIALPROTOCOL_H
+
+namespace SerialProtocol {
+    class SerialProtocol {
+    };
+} // SerialProtocol
+
+#endif //BOBBIEBOT_SERIALPROTOCOL_H
