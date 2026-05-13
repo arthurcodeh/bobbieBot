f0c7997b9264fbc86e22f02297412cb709821507
arthurcodeh <129370647+arthurcodeh@users.noreply.github.com>
2026-05-13 19:51:59 +0200

    [Remove:arduino] : file Limit
    - Je n'en ai pas besoin vue que je vérifie dans member si il y a une contrainte ou non sur mon servo

--- src/uno/config/Limits.h
+++
@@ -1,9 +0,0 @@
-// File: Limits.h
-// Role : Définition des limites pour les angles des servos
-// Description : Ce fichier contient les définitions des limites pour les angles des servos du robot
-
-#pragma once
-
-// Limites minimum et maximum pour les angles des servos (en degrés)
-#define SERVO_MIN_ANGLE 0
-#define SERVO_MAX_ANGLE 180
